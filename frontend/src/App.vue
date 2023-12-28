78ca27fea39a66646fac3b8cea37ffc861768ca5
Frederic Marvin Abraham <mail@fabraham.dev>
2023-12-28 17:49:35 +0100

    Start input component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld />
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-sheet
+          min-height="70vh"
+          rounded="lg"
+        >
+          <InputComponent />
+        </v-sheet>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+
+  import InputComponent from "@/components/InputComponent.vue";
 </script>
---
+++ frontend/src/components/InputComponent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import {ref} from 'vue'
+
+const input = ref('')
+
+const submit = () => {
+  console.log(input.value)
+}
+
+</script>
+
+<template>
+  <div>
+    <v-textarea
+      v-model="input"
+      label="Input"
+      outlined
+      rows="20"
+      class="mono-font text-center"
+    ></v-textarea>
+    <v-btn
+      class="w-100"
+      color="primary"
+      @click="submit"
+    >
+      Submit
+    </v-btn>
+  </div>
+</template>
+
+<style scoped>
+.mono-font {
+  font-family: 'Fira Code', monospace;
+}
+</style>
